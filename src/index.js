700ca1cb234e70cd43cd7cf88a7de8eb120a982e
Marco RR <ext_mareyes@falabella.cl>
2021-10-22 22:27:53 -0300

    ♻️ Pequeño refactor del index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const { getVODS } = require('./utils/parser');
-
 const FORMAT = process.argv?.[2]?.toUpperCase();
 
-async function printVODS() {
-  try {
-    const text = await getVODS(FORMAT);
-    console.log(text);
-  } catch (error) {
-    console.error(error);
-  }
+function printVODS() {
+  getVODS(FORMAT)
+    .then(console.log)
+    .catch(console.error);
 }
 
 (printVODS());
