ad505d4851d969420b080e84bcdce1fdf57f32cc
Marco <nervloverei@googlemail.com>
2022-05-02 13:24:12 -0400

    📝 Agrega JSDoc en utilitario de strings

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,16 +1,27 @@
 const { DEFAULT_VIDEO_TYPE } = require('../config');
 
+/**
+ * Limpia los caracteres especiales de un `string`.
+ *
+ * Ref: [Stack overflow](https://stackoverflow.com/a/37511463/2533051)
+ *
+ * Ref: [Carecteres diacriticos](https://en.wikipedia.org/wiki/Combining_Diacritical_Marks)
+ *
+ * Ref: [`normalize()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize#syntax)
+ * @param {string} str Un `string` cualquiera con caracteres especiales
+ * @returns {string} Un `string` limpio
+ */
 function normalize(str) {
   return str
-    .normalize('NFD')
-    .replace(/\p{Diacritic}/gu, '')
-    .toLowerCase(); // https://stackoverflow.com/a/37511463/2533051
+    .normalize('NFD') // Separa letras tildadas, ej: `â` -> `a` + `^`
+    .replace(/\p{Diacritic}/gu, '') // Elimina caracteres diacríticos, ej: `^`
+    .toLowerCase();
 }
 
 /**
  * Determina el tipo de un video dependiendo del titulo
- * @param {string} title
- * @returns {string} tipo de video de acuerdo al título
+ * @param {string} title El titulo de un sesion de la Convencion tal como viene desde la API de mediastream
+ * @returns {string} Tipo de video de acuerdo al título
  */
 function getType(title) {
   const types = [
@@ -30,19 +41,24 @@ function getType(title) {
 
 function has2Spaces(txt) { return txt.includes('  '); }
 
+/**
+ * Elimina algunos caracteres especificos del titulo de una sesion
+ * @param {string} title El titulo de un sesion de la Convencion tal como viene desde la API de mediastream
+ * @returns {string} Titulo limpio
+ */
 function cleanTitle(title) {
-  let clean = normalize(title);
-  clean = clean.replace('convencion constitucional', '');
-  clean = clean.replace('2021', '');
-  clean = clean.replace('2022', '');
-  clean = clean.replace(/-/g, ' ');
-  clean = clean.replace(/\"/g, '');
-  clean = clean.replace(/\'/g, '');
-  clean = clean.replace(/\,/g, ' ');
+  let titleClean = normalize(title)
+    .replace('convencion constitucional', '')
+    .replace('2021', '')
+    .replace('2022', '')
+    .replace(/-/g, ' ')
+    .replace(/\"/g, '')
+    .replace(/\'/g, '')
+    .replace(/\,/g, ' ');
   do {
-    clean = clean.replace(/\s\s/g, ' ');
-  } while (has2Spaces(clean));
-  return clean.trim();
+    titleClean = titleClean.replace(/\s\s/g, ' ');
+  } while (has2Spaces(titleClean));
+  return titleClean.trim();
 }
 
 module.exports = {
